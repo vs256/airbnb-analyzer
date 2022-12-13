0cbf496f49805e8390a5c09229544bec041ef680
vs256 <44126680+vs256@users.noreply.github.com>
2022-12-12 19:26:18 -0800

    backend support

--- frontend/src/Components/APIService.js
+++ frontend/src/Components/APIService.js
@@ -1,7 +1,7 @@
 export default class APIService {
   // Predict Values based on Specifications
   static InsertPredictValues(body) {
-    return fetch(`http://ec2-3-88-180-238.compute-1.amazonaws.com:5000/`, {
+    return fetch(`http://ec2-44-212-42-42.compute-1.amazonaws.com:5000/`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
